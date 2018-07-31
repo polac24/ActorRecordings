d9e9cebdb018ffd0be00c60b4cb52a99a976c4ba
Bartosz Polaczyk <polac24@gmail.com>
2018-07-31 20:11:08 +0200

    Initial Commit

---
+++ ActorRecordings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActorRecordings
+//
+//  Created by Bartosz Polaczyk on 31/07/2018.
+//  Copyright © 2018 Bartosz Polaczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
